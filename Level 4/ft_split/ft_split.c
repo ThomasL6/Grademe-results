ae7f98a1fb3d5c2ac3a52454744065bcfe22d2de
Thomas Lefebvre <thlefebv@c1r7p6.42lehavre.fr>
2023-08-01 10:34:16 +0200

    fix split

--- Level 4/ft_split/ft_split.c
+++ Level 4/ft_split/ft_split.c
@@ -5,23 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thlefebv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/31 16:17:54 by thlefebv          #+#    #+#             */
-/*   Updated: 2023/07/31 16:17:55 by thlefebv         ###   ########.fr       */
+/*   Created: 2023/08/01 09:50:58 by thlefebv          #+#    #+#             */
+/*   Updated: 2023/08/01 09:50:59 by thlefebv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-#include <stdio.h>
 
-char *ft_strncpy(char *s1, char *s2, int n)
+int ft_strncpy(char *s1, char *s2, int n)
 {
     int i = -1;
-    while(++i < n && s2[i] )
+    while(++i < n && s2[i])
         s1[i] = s2[i];
     s1[i] = '\0';
     return(s1);
 }
 
-char **ft_split(char *str)
+char    **ft_split(char *str)
 {
     int i = 0;
     int j = 0;
@@ -37,20 +36,21 @@ char **ft_split(char *str)
             i++;
     }
     char **tab = (char **)malloc(sizeof(char *) * (wc + 1));
+    if(!tab)
+        return(NULL);
     i = 0;
     while(str[i])
     {
-        while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
+        while(str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+            i++;
         j = i;
-        while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
+        while(str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+            i++;
         if(i > j)
         {
-            tab[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
+            tab[k] = (char*)malloc(sizeof(char) * ((i - j) + 1));
             ft_strncpy(tab[k++], &str[j], i - j);
         }
     }
-    tab[k] = NULL;
     return(tab);
 }
