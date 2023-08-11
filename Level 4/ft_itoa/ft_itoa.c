024055b559fe4732eb2339f33a1c63b71f5062c5
ThomasL6 <t.lefebvre53@gmail.com>
2023-08-11 10:48:38 +0200

    modif itoa

--- Level 4/ft_itoa/ft_itoa.c
+++ Level 4/ft_itoa/ft_itoa.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 char	*ft_itoa(int nbr)
 {
     if(nbr == -2147483648)
-        return("-2147483648\0");
+        return("-2147483648");
     int len = 0;
     int n = nbr;
-    if(nbr <= 0)
+    if(nbr <= 0)\
         len++;
     while(n)
     {
@@ -16,7 +17,6 @@ char	*ft_itoa(int nbr)
     char *result = malloc(sizeof(char) * (len + 1));
     if(!result)
         return(NULL);
-    result[len] = '\0';
     if(nbr == 0)
     {
         result[0] = '0';
@@ -30,7 +30,13 @@ char	*ft_itoa(int nbr)
     while(nbr)
     {
         result[--len] = nbr % 10 + '0';
-        nbr  = nbr / 10;
+        nbr = nbr / 10;
     }
     return(result);
 }
+
+/*int main()
+{
+    int str = 16;
+    printf("%s", ft_itoa(str));
+}*/
