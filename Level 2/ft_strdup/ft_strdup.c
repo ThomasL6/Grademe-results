b7d6b5bd0152b6dfdd294e2a8b6109d12b8b581e
Thomas Lefebvre <thlefebv@c2r11p6.42lehavre.fr>
2023-08-22 20:35:49 +0200

    dup

--- Level 2/ft_strdup/ft_strdup.c
+++ Level 2/ft_strdup/ft_strdup.c
@@ -1,16 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stdup.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thlefebv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 20:09:00 by thlefebv          #+#    #+#             */
+/*   Updated: 2023/08/22 20:09:02 by thlefebv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdlib.h>
 
 char    *ft_strdup(char *src)
 {
     int i = 0;
     char *tab;
-    tab = (char *)malloc(sizeof(char) * (i + 1))
+
+    tab = (char *)malloc(sizeof(char) * (i + 1));
     if(!tab)
         return(NULL);
+    i = 0;
     while(src[i])
     {
-        src[i] = tab [i];
+        tab[i] = src[i];
         i++;
     }
+    tab[i] = src[i];
     return(tab);
 }
